3549ad6dff6b1959e87af09c150dc1205923202e
Swarup Sapkota <empswarupcoc@gmail.com>
2024-05-13 08:29:37 +0545

    comments added

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,6 +1,7 @@
+// To get FAQ cards
 const faqCards = document.querySelectorAll('.faq-card');
 
-
+// To hide and show FAQ cards
 faqCards.forEach(card => {
   card.addEventListener('click', () => {
     card.classList.toggle('clicked');
