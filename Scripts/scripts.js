6c9d7f03485481e22b0000a3f07f41c0c70f50ab
Swarup Sapkota <empswarupcoc@gmail.com>
2024-05-13 15:08:20 +0545

    testimonial section responsive 1st phase

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -17,3 +17,47 @@ document.addEventListener('DOMContentLoaded', function() {
     navbarContainer.classList.toggle('show-nav');
   });
 });
+
+// For testimonial carousel
+const testimonialCards = document.querySelector('.testimonial-cards');
+let startX = 0;
+let isSwiping = false;
+let scrollLeft = 0;
+const cardWidth = testimonialCards.offsetWidth;
+const transitionDuration = 100; // Adjust as needed
+
+testimonialCards.addEventListener('touchstart', (e) => {
+  isSwiping = true;
+  startX = e.touches[0].clientX;
+  scrollLeft = testimonialCards.scrollLeft;
+});
+
+testimonialCards.addEventListener('touchmove', (e) => {
+  if (!isSwiping) return;
+  const currentX = e.touches[0].clientX;
+  const deltaX = startX - currentX;
+  testimonialCards.scrollLeft = scrollLeft + deltaX;
+});
+
+testimonialCards.addEventListener('touchend', () => {
+  isSwiping = false;
+});
+
+const testimonialControlLeft = document.querySelector('.testimonial-control-left');
+const testimonialControlRight = document.querySelector('.testimonial-control-right');
+
+testimonialControlLeft.addEventListener('click', () => {
+  testimonialCards.style.scrollBehavior = 'smooth'; // Enable smooth scrolling
+  setTimeout(() => {
+    testimonialCards.scrollLeft -= cardWidth;
+    testimonialCards.style.scrollBehavior = 'auto'; // Disable smooth scrolling after transition
+  }, transitionDuration);
+});
+
+testimonialControlRight.addEventListener('click', () => {
+  testimonialCards.style.scrollBehavior = 'smooth'; // Enable smooth scrolling
+  setTimeout(() => {
+    testimonialCards.scrollLeft += cardWidth;
+    testimonialCards.style.scrollBehavior = 'auto'; // Disable smooth scrolling after transition
+  }, transitionDuration);
+});
